6a4f5f73dfd4a890bd858565a3e114ccaed22704
visorcraft <thomas@visorcraft.com>
2026-07-18 11:39:34 -0500

    Release client train 0.60.3.
    Align package/library version and CI server download pins with MongrelDB
    engine release v0.60.3.

--- src/pkgIndex.tcl
+++ src/pkgIndex.tcl
@@ -6,4 +6,4 @@
 #
 # Licensing: MIT OR Apache-2.0.
 
-package ifneeded mongreldb 0.60.2 [list source [file join $dir mongreldb.tcl]]
+package ifneeded mongreldb 0.60.3 [list source [file join $dir mongreldb.tcl]]
